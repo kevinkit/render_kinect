9ef35ac762d6a9241037ce9ea4cc44482b791710
Kevin <kevinahoefle@gmail.com>
2017-06-15 14:47:20 +0200

    Will install dependencies for Ubuntu 14.04 and compile

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo apt-get install libeigen3-dev
+sudo apt-get install libpcl1
+sudo apt-get install libassimp-dev
+sudo apt-get install libnoise-dev
+sudo apt-get install libcgal-dev
+sudo apt-get install gcc-multilib
+sudo apt-get install libboost-all-dev
+sudo apt-get install libwebp-dev
+
+conda remove libtiff
+
+mkdir build
+cd build
+cmake ..
+make -j8
