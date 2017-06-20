31f21e46d8f07e17eb1041df7ed456fea6e0bd0b
Kevin <kevinahoefle@gmail.com>
2017-06-20 13:39:02 +0200

    Added check for LD_LIBRARY_PATH

--- install.sh
+++ install.sh
@@ -9,7 +9,52 @@ sudo apt-get install libwebp-dev
 
 #conda remove libtiff
 
-mkdir build
-cd build
-cmake ..
-make -j8
+#Get the LD_LIBRARY_PATH
+strindex() {
+	x="{$1%%$2*}"
+	[[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
+
+
+
+path=$LD_LIBRARY_PATH
+echo $path
+SOURCE="conda"
+SOURCEG="/usr/lib/x86_64-linux-gnu"
+
+if echo "$path" | grep -q "$SOURCEG"; then
+	echo "x86_64-linux-gnu is in LD_LIBRARY_PATH!"
+	if echo "$path" | grep -q "$SOURCE"; then
+		echo "Conda in library path found"
+
+		pos_c="$path" | grep -b -o conda
+		pos_g="$path" | grep -b -o /usr/lib/x86_64-linux-gnu
+
+		if [ $pos_g -gt $pos_c ]; then
+			echo "LD_LIBRARY_PATH is set up correct"
+      mkdir build
+      cd build
+      cmake ..
+      make -j8
+    else
+			echo "Please change LD_LIBRARY_PATH so, that the conda is at the ending of the path"
+		fi
+	else
+		echo "conda not found in LD_LIBRARY_PATH. If you want to install it at some point, conda must be added at the end"
+	fi
+else
+	echo "x86_64-linux-gnu is not in LD_LIBRARY_PATH!"
+        if echo "$path" | grep -q "$SOURCE"; then
+                echo "Conda in library path found"
+		echo "please add usr/lib/x86_64-linux-gnu to your LD_LIBRARY_PATH before conda"
+        else
+                echo "conda not found in library path"
+                echo "please add: /usr/lib/x86_64-linux-gnu to your LD_LIBRARY_PATH"
+        fi
+fi
+
+
+
+
+
+
